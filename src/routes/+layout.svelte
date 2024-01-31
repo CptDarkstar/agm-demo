d479890242a5a5bb2daeca0858c40d688f23ce8a
CptDarkstar <137180606+CptDarkstar@users.noreply.github.com>
2024-01-31 14:30:00 +0200

    Log out if page closes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,42 +7,50 @@
   const nonAuthRoutes = ["/"];
   
   onMount(() => {
-    console.log("Mounting!");
-    const unsubscribe = auth.onAuthStateChanged(async user => {
-      const currentPath = window.location.pathname;
-      
-      if (!user && !nonAuthRoutes.includes(currentPath)) {
+ console.log("Mounting!");
+ const unsubscribe = auth.onAuthStateChanged(async user => {
+    const currentPath = window.location.pathname;
+
+    if (user) {
+      sessionStorage.setItem('loggedIn', 'true'); // User is logged in
+
+      if (currentPath === "/") {
+        window.location.href = "/dashboard";
+        return;
+      }
+
+      const docRef = doc(db, 'users', user.uid)
+      const docSnap = await getDoc(docRef)
+      if (!docSnap.exists()) {
+        const userRef = doc(db, 'user', user.uid)
+        await setDoc(
+          userRef,
+          {
+            email: user.email, 
+          }
+        );
+      } else {
+        const userData = docSnap.data()
+      }
+    } else {
+      sessionStorage.removeItem('loggedIn'); // User is not logged in
+
+      if (!nonAuthRoutes.includes(currentPath)) {
         if (window.location.pathname !== "/") {
           window.location.href = "/";
           return;
         }
       }
+    }
 
-      if (user && currentPath === "/") {
-        window.location.href = "/dashboard";
+    // Check if the user is logged in
+    if (!user && sessionStorage.getItem('loggedIn') === 'true') {
+        sessionStorage.removeItem('loggedIn');
+        window.location.href = "/";
         return;
-      }
-
-      if (!user) {
-        return;
-      }
-
-      const docRef = doc(db, 'users', user.uid)
-            const docSnap = await getDoc(docRef)
-            if (!docSnap.exists()) {
-                const userRef = doc(db, 'user', user.uid)
-                await setDoc(
-                    userRef,
-                    {
-                        email: user.email, 
-                    }
-                );
-            } else {
-                const userData = docSnap.data()
-
-            }
-    });
-  });
+    }
+ });
+});
 </script>
 
 <div class="mainContainer">
