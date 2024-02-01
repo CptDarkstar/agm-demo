9c2e2d3bc33d89db8cd689c74cd386f2acc87412
CptDarkstar <137180606+CptDarkstar@users.noreply.github.com>
2024-02-01 15:43:29 +0200

    Forgot password page style

--- src/routes/forgot_password/+page.svelte
+++ src/routes/forgot_password/+page.svelte
@@ -1,8 +1,9 @@
 <script>
+    import '@fortawesome/fontawesome-free/css/all.min.css';
     import { getAuth, sendPasswordResetEmail } from "firebase/auth";
 
     let email = '';
-    
+
     function forgotPassword() {
         const auth = getAuth();
         sendPasswordResetEmail(auth, email)
@@ -12,10 +13,30 @@
     };
 </script>
 
-<div>
-    <form on:submit|preventDefault={forgotPassword}>
-        <input type="email" bind:value={email} placeholder="Enter your email" required />
-        <button type="submit">Reset Password</button>
-   </form>
+<div class="main">
+    <div class="content">
+        <img class="logo" src="rnslogo.png" alt="RNS logo">
+        <form on:submit|preventDefault={forgotPassword}>
+            <p>Please enter your email to reset your password</p>
+            <input type="email" bind:value={email} placeholder="Enter your email" required />
+            <div class="button-group">
+                <button onclick="history.back()">Go Back</button>
+                <button type="submit">Reset Password</button>
+            </div>
+        </form>
+    </div>
 </div>
-   
+
+<style>
+    .content{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        font-family: 'Merriweather', sans-serif;
+    }
+    form{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
